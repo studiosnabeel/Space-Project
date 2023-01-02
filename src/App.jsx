873d43f843699c4d414c34dea096bd3365d11cfe
MosDevx <mosesk.wamae@gmail.com>
2023-01-02 14:58:56 +0300

    Make Header Component; Implement NavLink

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./Components/Header";
+import Profile from "./Components/Profile";
+import Rockets from "./Components/Rockets";
+import Missions from "./Components/Missions";
+import { Routes,Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Rockets/>}/>
+        <Route path='/rockets' element={<Rockets/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/missions' element={<Missions/>}/>
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import PlanetLogo from '../Assets/planet.png'
+
+
+const Header = () => {
+	return (
+		
+			<header className=' flex justify-between items-center p-2 px-6 md:px-12 m-auto'>
+			
+				<div className='flex justify-between items-center m-auto md:m-0'>
+					 <img className='w-14 h-14 md:w-16 md:h-16' src={PlanetLogo} alt="Planet Logo" /> 
+					 <h2 className='font-bold text-2xl md:text-3xl pl-4 '>Space Travelers' Hub</h2> </div>
+				
+				<nav className='hidden md:flex justify-between text-xl items-center text-sky-600 w-[300px]'>
+					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/rockets'>Rockets</NavLink>
+					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/missions'>Missions</NavLink>
+					<div className='w-0.5 h-4 bg-black'></div>
+					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/profile'>My Profile</NavLink> 
+				</nav>
+			
+			</header>	
+	
+	)
+}
+
+export default Header
