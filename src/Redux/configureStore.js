7f055f2ffd18498d9a0306f77f33534986d0ef3b
SekoVper <damehlivingstone1@gmail.com>
2023-01-03 16:42:50 +0000

    Fix linter errors.

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './missionSlice';
 
-
 import rocketReducer from './rocketSlice';
 
 export default configureStore({
   reducer: {
-    rockets: rocketReducer, 
-    missions: missionReducer
+    rockets: rocketReducer,
+    missions: missionReducer,
   },
 });
-
--- src/Redux/missionSlice.js
+++ src/Redux/missionSlice.js
@@ -1,24 +1,47 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign */
+/* eslint-disable camelcase */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   missions: [],
 
   status: 'idle',
-  error: null
-}
+  error: null,
+};
 
-const MISSIONS_API_URL = 'https://api.spacexdata.com/v3/missions'
+const MISSIONS_API_URL = 'https://api.spacexdata.com/v3/missions';
+
+export const getMissionsData = createAsyncThunk('missions/getMissionsData', async () => {
+  const res = await fetch(MISSIONS_API_URL);
+  const data = await res.json();
+  const newMissionsArray = [];
+
+  data.forEach((mission) => {
+    const {
+      mission_id,
+      mission_name,
+      description,
+    } = mission;
+    const newMission = {
+      id: mission_id,
+      title: mission_name,
+      desc: description,
+    };
+    newMissionsArray.push(newMission);
+  });
+  return newMissionsArray;
+});
 
 const missionSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {
-    joinMission(action, state){
-      console.log(action, state)
-    },
-    leaveMission(action, state){
-      console.log(action, state)
-    }
+    // joinMission(action, state) {
+    //   console.log(action, state);
+    // },
+    // leaveMission(action, state) {
+    //   console.log(action, state);
+    // },
   },
 
   extraReducers(builder) {
@@ -29,37 +52,13 @@ const missionSlice = createSlice({
       .addCase(getMissionsData.fulfilled, (state, action) => {
         state.status = 'succeeded';
         state.missions = [...action.payload];
-        console.log('in the extra reducers', state.missions);
       })
       .addCase(getMissionsData.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
   },
-})
+});
 
-export const getMissionsData = createAsyncThunk('missions/getMissionsData', async () => {
-  const res = await fetch(MISSIONS_API_URL);
-  const data = await res.json();
-  const newMissionsArray = [];
-
-    data.forEach((mission) => {
-      const {
-        mission_id,
-        mission_name,
-        description,
-      } = mission;
-      const newMission = {
-        id: mission_id,
-        title: mission_name,
-        desc: description,
-      };
-      newMissionsArray.push(newMission);
-    });
-    console.log(newMissionsArray);
-    return newMissionsArray;
-})
-
-
-export const {joinMission, leaveMission} = missionSlice.actions
+export const { joinMission, leaveMission } = missionSlice.actions;
 export default missionSlice.reducer;
