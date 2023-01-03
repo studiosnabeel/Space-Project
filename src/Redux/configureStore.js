135dd2b894937aa41a8568b5b50bc9ead7bf1d46
SekoVper <damehlivingstone1@gmail.com>
2023-01-03 16:11:41 +0000

    Add missions reducer Combined reducers

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionReducer from './missionSlice';
+
 
 import rocketReducer from './rocketSlice';
 
 export default configureStore({
   reducer: {
-    rockets: rocketReducer,
+    rockets: rocketReducer, 
+    missions: missionReducer
   },
 });
