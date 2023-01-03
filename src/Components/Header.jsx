23dbf07e7760c823adcee936663d065767b8fe46
Nabeel Akbar <studiosnabeel@gmail.com>
2023-01-03 13:16:15 +0500

    Change nav width

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
 			<header className=' flex justify-between items-center p-2 px-6 md:px-12 m-auto'>
 			
 				<div className='flex justify-between items-center m-auto md:m-0'>
-					 <img className='w-14 h-14 md:w-16 md:h-16' src={PlanetLogo} alt="Planet Logo" /> 
-					 <h2 className='font-bold text-2xl md:text-3xl pl-4 '>Space Travelers' Hub</h2> </div>
+					 <img className='w-14 h-14' src={PlanetLogo} alt="Planet Logo" /> 
+					 <h2 className='font-bold text-xl md:text-2xl pl-4 '>Space Travelers' Hub</h2> </div>
 				
-				<nav className='hidden md:flex justify-between text-xl items-center text-sky-600 w-[400px]'>
+				<nav className='hidden md:flex justify-between text-lg items-center text-sky-600 w-[400px]'>
 					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/rockets'>Rockets</NavLink>
 					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/dragons'>Dragons</NavLink>
 					<NavLink className={({isActive})=> isActive ? "underline underline-offset-2" : undefined} to='/missions'>Missions</NavLink>
