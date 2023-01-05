aa5eb3150e5bd85a48fa4a6ec7aa72442024545d
MosDevx <mosesk.wamae@gmail.com>
2023-01-05 19:17:41 +0300

    Made changes to styling. Display reserved pill and Button changes

--- src/Components/Dragons/Dragons.jsx
+++ src/Components/Dragons/Dragons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 /* eslint-disable react/prop-types */
 const Dragons = ({
-  image, title, desc,
+  image, title, desc, reserved,
 }) => (
   <div className="mt-2 mb-4 flex mx-12 items-start">
     <div className=" flex items-center w-[200px] h-[170px] lg:w-[400px] lg:h-[300px] flex-none">
@@ -15,13 +15,25 @@ const Dragons = ({
     <div className=" mx-2">
       <h3 className="font-bold text-lg">{title}</h3>
       <p>
+        {reserved && (
+        <span
+          className="
+            rounded bg-sky-400 text-center px-2 mr-2 text-white"
+        >
+          reserved
+        </span>
+        )}
         {desc}
       </p>
       <button
         type="button"
-        className="bg-[#007bff] text-white px-3 py-1.5 rounded mt-2 "
+        className={
+          reserved
+            ? 'drop-shadow-md border-2 border-slate-400 px-3 text-slate-800 py-1.5 rounded mt-2'
+            : 'bg-[#007bff] text-white px-3 py-1.5 rounded mt-2 '
+        }
       >
-        Reserve rocket
+        {reserved ? 'Cancel Reservation' : 'Reserve rocket'}
       </button>
     </div>
   </div>
