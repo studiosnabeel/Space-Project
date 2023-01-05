3d5944f4241738cff31dc119e63c61a420fb495f
MosDevx <mosesk.wamae@gmail.com>
2023-01-05 21:22:46 +0300

    Display Reserved Dragons in Profile page

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { selectReserveDragons } from '../Redux/dragonSlice';
 import { selectReserveMissions } from '../Redux/missionSlice';
 import { selectReserveRockets } from '../Redux/rocketSlice';
 
 const Profile = () => {
   const reservedRockets = useSelector(selectReserveRockets);
   const reservedMissions = useSelector(selectReserveMissions);
+  const reservedDragons = useSelector(selectReserveDragons);
+
   return (
     <div className="flex justify-between mt-2">
       <div className="flex flex-col w-1/3 mx-4">
@@ -26,7 +29,11 @@ const Profile = () => {
       </div>
       <div className="flex flex-col w-1/3 mx-2">
         <h3 className="font-bold">My Dragons</h3>
-        <p className="border p-2">dragonfly</p>
+        {reservedDragons.map((dragon) => (
+          <p key={dragon.id} className="border p-2">
+            {dragon.title}
+          </p>
+        ))}
       </div>
     </div>
   );
