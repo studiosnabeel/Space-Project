b5abf7a466118a54be53cf29bdaec2b5766f9384
MosDevx <mosesk.wamae@gmail.com>
2023-01-05 13:56:24 +0300

    Modify Dragon component

--- src/Components/Dragons.jsx
+++ src/Components/Dragons.jsx
@@ -1,18 +1,7 @@
+import React from 'react';
 
-const Dragons = () => {
-	return (
-		<div>Dragons</div>
-		<div className='flex mx-12 '>
-        <div className='flex items-center'>
-	      <img className='w-12 h-12' src="" alt="picture1" />
-	</div>
-		<div className='mx-2'>
-			<h3 className='font-bold text-lg'>Dragon 1</h3>
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti sequi ex nesciunt aut quod rerum aspernatur ipsam eos ut tenetur.</p>
-			<button className='bg-[#007bff] text-white px-3 py-1.5 rounded mt-2'>
-				Reserve Dragon 
-			</button>
-			</div>
-		</div>	
-	)
-}
+const Dragons = () => (
+  <div>Dragons</div>
+);
+
+export default Dragons;
