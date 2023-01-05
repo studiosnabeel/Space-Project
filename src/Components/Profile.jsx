5db08c109c1e12e4f4b1b1998c0b9c9a992c9f02
SekoVper <damehlivingstone1@gmail.com>
2023-01-05 11:54:15 +0000

    Populate the missions section with join missions data

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { selectReserveMissions } from '../Redux/missionSlice';
 import { selectReserveRockets } from '../Redux/rocketSlice';
 
 const Profile = () => {
   const reservedRockets = useSelector(selectReserveRockets);
+  const reservedMissions = useSelector(selectReserveMissions);
   return (
     <div className="flex justify-between mt-2">
       <div className="flex flex-col w-1/3 mx-4">
         <h3 className="font-bold">My Missions</h3>
+        {reservedMissions.map((mission) => (
+          <p key={mission.id} className="border p-2">
+            {mission.title}
+          </p>
+        ))}
       </div>
       <div className="flex flex-col w-1/3 mx-2">
         <h3 className="font-bold">My Rockets</h3>
