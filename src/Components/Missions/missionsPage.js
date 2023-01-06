1d796a70f8fbf4b71b34008563636bb4cfecaf55
SekoVper <damehlivingstone1@gmail.com>
2023-01-06 11:42:50 +0000

    Modify missions file

--- src/Components/Missions/missionsPage.js
+++ src/Components/Missions/missionsPage.js
@@ -11,7 +11,7 @@ const MissionsPage = () => {
     if (missions.missions.length === 0) {
       dispatch(getMissionsData());
     }
-  }, [dispatch]);
+  }, [dispatch], missions.missions.length);
 
   return (
     missions.status === 'loading' ? <h2>Loading</h2> : <MissionsList missions={missions.missions} />
