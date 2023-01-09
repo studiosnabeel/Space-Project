9004e924f138dbb484e6b293b23f93962bdd1368
SekoVper <damehlivingstone1@gmail.com>
2023-01-09 10:34:47 +0000

    Modify README and Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<DisplayDragons />} />
+        <Route path="/" element={<DisplayRockets />} />
         <Route path="/rockets" element={<DisplayRockets />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/missions" element={<MissionsPage />} />
